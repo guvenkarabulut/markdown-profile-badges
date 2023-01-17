ecab9a313007d9f3abc0de694414e245fd8e3425
Güven Karabulut <guven.07.568@gmail.com>
2023-01-17 20:04:29 +0300

    some changes

--- Python Files/simpleIcons.py
+++ Python Files/simpleIcons.py
@@ -3,33 +3,28 @@ import os
 
 dosya = open("brandAndIcons.md", "w")
 
-dosya.write(
-    "| Brand | Flat Icon | Plastic Icon | Squared Bold Icon | Squared Icon | Markdown \n")
-
-dosya.write("| --- | --- | --- | --- | --- | --- | \n")
 if os.path.exists("allBrandInSimpleIcons.txt"):
     with open("allBrandInSimpleIcons.txt", "r") as f:
+        firstLetter = f.readline().strip()
+        dosya.write(
+            f"<details> <summary>{firstLetter}</summary><table><tr><th>Brand</th><th>Squared Bold Icon</th><th>Markdown </th></tr>")
         for line in f:
+            if firstLetter.upper() != line[0].upper():
+                dosya.write("</table></details>")
+                firstLetter = line[0]
+                dosya.write(
+                    f"<details> <summary>{firstLetter}</summary><table><tr><th>Brand</th><th>Squared Bold Icon</th><th>Markdown </th></tr>")
             try:
                 icon = icons.get(line.strip())
-                dosya.write(f"| {icon.title} | ")
-                dosya.write
-                dosya.write(
-                    f"![{icon.title}](https://img.shields.io/badge/{icon.slug}-{icon.hex}.svg?style=flat&logo={icon.slug}&logoColor=white )|"
-                )
-                dosya.write(
-                    f"![{icon.title}](https://img.shields.io/badge/{icon.slug}-{icon.hex}.svg?style=plastic&logo={icon.slug}&logoColor=white&)|"
-                )
-                dosya.write(
-                    f"![{icon.title}](https://img.shields.io/badge/{icon.slug}-{icon.hex}.svg?style=for-the-badge&logo={icon.slug}&logoColor=white)|"
-                )
-                dosya.write(
-                    f"![{icon.title}](https://img.shields.io/badge/{icon.slug}-{icon.hex}.svg?style=flat-square&logo={icon.slug}&logoColor=white)|"
-                )
-                dosya.write(
-                    f"```![{icon.title}](https://img.shields.io/badge/{icon.slug}-{icon.hex}.svg?style=<STYLE_NAME>&logo={icon.slug}&logoColor=white)```|"
-                )
-                dosya.write("\n")
+                dosya.write("<tr>"
+                            + f"<td>{icon.title}</td>"
+                            + f"<td><img src='https://img.shields.io/badge/{icon.slug}-{icon.hex}.svg?style=for-the-badge&logo={icon.slug}&logoColor=white' /></td>"
+                            + f"<td>![{icon.title}](https://img.shields.io/badge/{icon.slug}-{icon.hex}.svg?style=[style_name]&logo={icon.slug}&logoColor=white)</td>"
+                            + "</tr>"
+                            + "\n"
+                            )
             except:
                 pass
+dosya.write("</table></details>")
+
 dosya.close()
